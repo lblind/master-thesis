e9905ef41a90a02e41e869864f0b9670885850a6
L. Blind <l.blind@student.maastrichtuniversity.nl>
2022-06-19 20:55:36 +0200

    CSVs vam + selection time period netCDF

--- src/main.py
+++ src/main.py
@@ -5,26 +5,25 @@ MAIN
 Starting point of program execution
 """
 import pandas as pd
-
 import preprocessing as preproc
 
 
 if __name__ == "__main__":
 
-    # 1. Read food prices, convert to excel and return as dfs
-    df_central, df_north, df_south = preproc.read_and_clean_csvs_food_prices()
+    # # 1. Read food prices, convert to excel and return as dfs
+    # df_central, df_north, df_south = preproc.read_and_clean_csvs_food_prices()
+    #
+    # path_to_longs = "../input/food-price-dta/longs and lats/MWI_markets.csv"
+    #
+    # df_markets_coord = pd.read_csv(path_to_longs)
+    # df_markets_coord.rename(columns={'MarketName': 'Market'}, inplace=True)
+    #
+    # df_all = pd.merge(df_north, df_markets_coord, on="Market", how="inner")
+    # print(df_all)
 
-    path_to_longs = "../input/food-price-dta/longs and lats/MWI_markets.csv"
 
-    df_markets_coord = pd.read_csv(path_to_longs)
-    df_markets_coord.rename(columns={'MarketName': 'Market'}, inplace=True)
-
-    df_all = pd.merge(df_north, df_markets_coord, on="Market", how="inner")
-    print(df_all)
-
-
-    # path_climate = "../input/climate-dta/spei01.nc"
-    # df_spei = preproc.read_climate_data(path_climate)
+    path_climate = "../input/climate-dta/spei01.nc"
+    df_spei = preproc.read_climate_data(path_climate)
     # # match df all to df climate
     # df_all_climate = pd.merge(df_all, df_spei, on=["long", "lat"], how="inner")
 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 import xarray as xr
 import netCDF4
+import numpy as np
 
 
 def read_and_clean_csvs_food_prices(path_to_dir_csvs="../input/food-price-dta/csv"):
@@ -57,7 +58,7 @@ def read_and_clean_csvs_food_prices(path_to_dir_csvs="../input/food-price-dta/cs
     return dfs
 
 
-def read_climate_data(path_to_netcdf):
+def read_climate_data(path_to_netcdf, start_time = "2003-01-01", end_time="2022-01-07"):
     """
     Reads the netcdf and converts it into a pandas df
 
@@ -65,76 +66,86 @@ def read_climate_data(path_to_netcdf):
     :return:
     """
     ds = xr.open_dataset(path_to_netcdf)
-    print("\nKeys\n")
-    print(ds.keys())
+    # print(ds)
 
-    data = netCDF4.Dataset(path_to_netcdf)
+    # Just keep data that is in correct time range
+    ds = ds.sel(time=slice(start_time, end_time))
 
-    # Extract variables
-    print("Variables")
-    columns = ["long", "lat", "spei", "time",]
-    # print(data.variables["lon"])
+    print(ds)
 
-    print("Dimensions", ds.dims)
-    print("Variables", ds.variables)
+    df_climate = pd.DataFrame()
 
-    # extract the data per column
-    long = data.variables["lon"][:]
-    lat = data.variables["lat"][:]
-    time = data.variables["time"][:]
-    spei = data.variables["spei"][:]
-
-    print(f"LONG\n{long}, {len(long)}")
-    print(f"LAT\n{lat}, {len(lat)}")
-
-    # make df
-    # df_new = pd.DataFrame(data=[long, lat, spei], index=time, columns=columns[:-1])
-    # print("\nDFNEW\n", df_new.columns, "\n", df_new)
-
-    path_logs_dir = "../input/climate-dta/logs"
-
-    if os.path.exists(path_logs_dir) is False:
-        os.makedirs(path_logs_dir)
-
-    # store head of cdf as excel
-    # ds.head().to_dataframe().to_excel(f"{path_logs_dir}/header_cdf.xlsx")
-
-    df = ds.to_dataframe()
-
-    # drop values with no spei available
-    df=df.dropna()
-
-    # drop everything that doesn't match the WFP climate base (-2020)
-
-
-    print(df)
-    print(df.shape)
-    # print(df.lon)
-
-    # df.iloc[:50, :].to_excel("../input/climate-dta/subsection_spei.xlsx")
-
-
-
+    # print("\nKeys\n")
+    # print(ds.keys())
     #
-    # print(df.columns)
+    # data = netCDF4.Dataset(path_to_netcdf)
+    #
+    # # Extract variables
+    # print("Variables")
+    # columns = ["long", "lat", "spei", "time",]
+    # # print(data.variables["lon"])
+    #
+    # print("Dimensions", ds.dims)
+    # print("Variables", ds.variables)
+    #
+    # # extract the data per column
+    # long = data.variables["lon"][:]
+    # lat = data.variables["lat"][:]
+    # time = data.variables["time"][:]
+    # spei = data.variables["spei"][:]
+    #
+    # print(f"LONG\n{long}, {len(long)}")
+    # print(f"LAT\n{lat}, {len(lat)}")
+    # print(f"Time\n{time}")
+    #
+    # # make df
+    # # df_new = pd.DataFrame(data=[long, lat, spei], index=time, columns=columns[:-1])
+    # # print("\nDFNEW\n", df_new.columns, "\n", df_new)
+    #
+    # path_logs_dir = "../input/climate-dta/logs"
+    #
+    # if os.path.exists(path_logs_dir) is False:
+    #     os.makedirs(path_logs_dir)
+    #
+    # # store head of cdf as excel
+    # # ds.head().to_dataframe().to_excel(f"{path_logs_dir}/header_cdf.xlsx")
+    #
+    # df = ds.to_dataframe()
+    #
+    # # drop values with no spei available
+    # df=df.dropna()
+    #
+    # # drop everything that doesn't match the WFP climate base (-2020)
+    #
+    #
     # print(df)
+    # print(df.shape)
+    # # print(df.lon)
     #
-    # print(f"------------------------------\n"
-    #       f"After reindexing\n"
-    #       f"------------------------------\n")
+    # # df.iloc[:50, :].to_excel("../input/climate-dta/subsection_spei.xlsx")
     #
-    # # REINDEX dataframe
-    # df.reindex(columns=columns)
-    # print(df.columns)
-    # print(df)
-
-    # print first column
-    # print(df.iloc[:, 0])
-
-    # print first row
-    # print(df.iloc[0])
-
-    return df
+    #
+    #
+    # #
+    # # print(df.columns)
+    # # print(df)
+    # #
+    # # print(f"------------------------------\n"
+    # #       f"After reindexing\n"
+    # #       f"------------------------------\n")
+    # #
+    # # # REINDEX dataframe
+    # # df.reindex(columns=columns)
+    # # print(df.columns)
+    # # print(df)
+    #
+    # # print first column
+    # # print(df.iloc[:, 0])
+    #
+    # # print first row
+    # # print(df.iloc[0])
+    #
+    # return df
 
 
 
