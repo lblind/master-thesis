eca5f825aef78f42e46db7ee700a4efd0031d706
L. Blind <l.blind@student.maastrichtuniversity.nl>
2022-07-10 20:41:31 +0200

    Separate module for dataset creation

---
+++ src/dataset_creation.py
@@ -0,0 +1,113 @@
+"""
+CREATION OF DATASET
+-------------------
+
+"""
+import preprocessing as preproc
+
+
+def create_dataset(country, dropped_commodities):
+    """
+
+    :param country:
+    :param dropped_commodities:
+    :return:
+    """
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: PHASE 1 (MERGE DATA)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
+    # PART A) Get food price part of data
+    # -----------------------------------
+
+    # 1. Read food prices, convert to excel and return as df
+    df_wfp = preproc.get_df_wfp_preprocessed_excel_per_country(country=country, dropped_commodities=dropped_commodities)
+    # df_wfp = preproc.get_df_wfp_preprocessed_excel_per_region(country=country, dropped_commodities=dropped_commodities)
+
+    # 2. Read CSV containing market coordinates and merge to price data
+    df_wfp_with_coords = preproc.read_and_merge_wfp_market_coords(df_wfp=df_wfp, country=country)
+    # 3. Preparation for merge with Part B): Extract range of 3 main variables: time, longitude, latitude
+    slice_time, slice_lon, slice_lat = preproc.extract_time_lon_lat_slice(df_wfp_with_coords)
+
+    print(f"Slice Time: {slice_time}\nSlice Lon: {slice_lon}\nSlice Lat: {slice_lat}")
+
+    # PART B) Get climate part of data (SPEI)
+    # ---------------------------------------
+
+    df_spei = preproc.read_climate_data(time_slice=slice_time, long_slice=slice_lon, lat_slice=slice_lat,
+                                        country=country)
+
+    # PART C) Merge Outcomes of Part A) and Part C) and store them as excel
+    # ---------------------------------------------------------------------
+    df_final = preproc.merge_food_price_and_climate_dfs(df_wfp_with_coords=df_wfp_with_coords, df_spei=df_spei)
+
+    # Check that everything went okay in preprocessing/ merge part
+    if df_final.shape[0] != df_wfp.shape[0]:
+        raise ValueError(f"Something went wrong in the preprocessing part.\n"
+                         f"# rows of final/ merged df {df_final.shape[0]} should"
+                         f" be the same as for the one of wfp {df_wfp.shape[0]}")
+
+    df_final = preproc.classify_droughts(df_final)
+
+    # Summary statistics categorical
+    counts = df_final["SpeiCat"].value_counts()
+    print(f"Counts:\n{counts}")
+
+    counts_drought = df_final["Drought"].value_counts()
+    print(f"Counts Drought:\n{counts_drought}")
+    print(f"Share of droughts: {counts_drought[True]}/ {(counts_drought[True] + counts_drought[False])}")
+
+    # Get the distinguished datasets
+    df_drought, df_no_drought = preproc.separate_df_drought_non_drought(df_final)
+
+    # Check missings
+    preproc.summary_stats_prices_droughts(df_final=df_final)
+    # preproc.summary_stats_missings(df_final=df_wfp)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # TAKE CARE OF MISSING DATA -> SUBSET
+    # ------------------------------------------------------------------------------------------------------------------
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: PHASE 2 (MISSING DATA: SUBSET CREATION)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: PHASE 2.1 (DROUGHTS)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+    # drop 2021, 2022 as no data for droughts (/SPEI) is available for those (even though WFP data is)
+    years_to_drop = [2021, 2022]
+    print(f"Dropping years: {years_to_drop}")
+    df_final = preproc.drop_years(df_final=df_final, years_list=years_to_drop)
+
+    preproc.summary_stats_prices_droughts(df_final=df_final, excel_output_extension="-preproc-2")
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: PHASE 2.2 (PRICES)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
+    # 65 (30%), 60 (27%, 30% also for central region)
+    cut_off_percentile = 60
+
+    # Cut all regions with missing >= cut_off_percentile of missing values
+    df_final = preproc.drop_missing_decile_per_region_prices(
+        path_excel_sum_stats=f"../output/{country}/summary-statistics/{country}-"
+                             f"sum-stats-preproc-2.xlsx",
+        df_final=df_final,
+        cut_off_percentile=cut_off_percentile, excel_output_extension=f"-{cut_off_percentile}p")
+
+    # Write sum stats
+    preproc.summary_stats_prices_droughts(df_final=df_final, excel_output_extension=f"-preproc-3-{cut_off_percentile}p")
+
+    # EXTRAPOLATE REGIONAL PATTERNS
+    # doesn't extrapolate tails for interpolation method: cubic
+    df_final = preproc.extrapolate_prices_regional_patterns(df_final=df_final, interpolation_method="linear")
+
+    # Write sum stats
+    preproc.summary_stats_prices_droughts(df_final=df_final, excel_output_extension=f"-preproc-4-{cut_off_percentile}p")
+
+    preproc.write_preprocessing_results_to_excel(df_wfp=df_wfp, df_wfp_with_coords=df_wfp_with_coords,
+                                                 df_spei=df_spei, df_final=df_final, df_drought=df_drought,
+                                                 df_no_drought=df_no_drought)
+
+    return df_final
--- src/visualization.py
+++ src/visualization.py
@@ -4,6 +4,8 @@ VISUALIZATION
 
 """
 
+# TODO: color regions
+# TODO: visualize prices (e.g. color scale, kdeplot)
 # from mpl_toolkits.basemap import Basemap
 
 import geoplot as gplt
@@ -21,6 +23,12 @@ import numpy as np
 
 def plot_malawi(df_final):
     """
+
+    Structure Shape File
+    NAME_0: Name of country
+    NAME_1: Name of bigger regions
+    NAME_2: Name of cities
+
 
     References
     ----------
@@ -45,16 +53,37 @@ def plot_malawi(df_final):
     malawi_adm2 = gpd.read_file("../input/Malawi/maps/diva-gis/MWI_adm/MWI_adm2.shp")
     malawi_adm3 = gpd.read_file("../input/Malawi/maps/diva-gis/MWI_adm/MWI_adm3.shp")
 
+    malawi_adm1.rename(columns={"NAME_1" : "Market"}, inplace=True)
+
+    # merge adm1 data (for now) with df_final
+    df_final_merged = malawi_adm1.merge(df_final, left_on="Market", right_on="Market")
+
     # pure country
     # malawi_adm0.plot()
     # adm1 -> regions?
     # malawi_adm1.plot()
-    print(malawi_adm1.columns)
+    print(malawi_adm3.columns)
     # adm2 -> more finegranular (cities?)
     # malawi_adm2.plot()
     # malawi_adm3.plot()
 
 
+
+    # # print(malawi_adm1.NAME_1)
+    # print("NAME 0\n", malawi_adm3.NAME_0.unique())
+    # print("NAME 1\n", malawi_adm3.NAME_1.unique(), len(malawi_adm3.NAME_1.unique()))
+    # print("NAME 2\n",malawi_adm3.NAME_2.unique(), len(malawi_adm3.NAME_2.unique()))
+    # print("NAME 3\n",malawi_adm3.NAME_3.unique())
+    # # Merge df_final with shape dataset
+    #
+    # print("ID 0\n", malawi_adm3.ID_0.unique())
+    # print("ID 1\n", malawi_adm3.ID_1.unique(), len(malawi_adm3.ID_1.unique()))
+    # print("ID 2\n", malawi_adm3.ID_2.unique(), len(malawi_adm3.ID_2.unique()))
+    # print("ID 3\n", malawi_adm3.ID_3.unique(), len(malawi_adm3.ID_3.unique()))
+    #
+    # print("NL NAME 3 3\n", malawi_adm3.NL_NAME_3.unique(), len(malawi_adm3.NL_NAME_3.unique()))
+
+    # print(malawi_adm3)
 
     # convert regular dataframe to geopandas df
     # gdf_final = gpd.GeoDataFrame(
@@ -64,7 +93,19 @@ def plot_malawi(df_final):
         df_final, geometry=gpd.points_from_xy(df_final.MarketLongitude, df_final.MarketLatitude)
     )
     ax = gplt.polyplot(malawi_adm1)
+    # ax = gplt.polyplot(df_final_merged, hue="Region")
     # ax = gplt.polyplot(malawi_adm1, projection=gcrs.AlbersEqualArea())
+    print(df_final_merged.head())
+    gplt.choropleth(
+        df_final_merged,
+        hue="Region",
+        edgecolor="white",
+        linewidth=1,
+        cmap="Greens",
+        legend=True,
+        scheme="FisherJenks",
+        ax=ax
+    )
 
     gplt.pointplot(gdf_final, ax=ax, hue="Region")
     # gplt.pointplot(gdf_final, ax=ax)
@@ -76,3 +117,12 @@ def plot_malawi(df_final):
     plt.legend()
     plt.savefig("../output/Malawi/plots/Map-Markets.png")
     plt.show()
+
+
+def visualize_prices(df_final):
+    """
+
+    :param df_final:
+    :return:
+    """
+    pass
