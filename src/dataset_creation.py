01d47e33a4c2f4bdf37fce2336aca89dd90488a8
L. Blind <l.blind@student.maastrichtuniversity.nl>
2022-07-16 20:56:23 +0200

    State of the art before preprocessing per Commodity

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -32,12 +32,24 @@ def create_dataset(country, dropped_commodities):
                                                                  dropped_commodities=dropped_commodities)
     df_wfp.to_excel(f"../output/{country}/intermediate-results/df_wfp.xlsx")
 
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Check that all combinations are filled with nans (if no data given)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
     # check validity of extracted dataset (check that for all combinations of markets, commodity & time
     # food prices have been populated)
     df_wfp = preproc.check_markets_per_commodity_time(df_wfp=df_wfp)
 
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Adjust food prices to inflation (last recent (food) inflation level)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
     # Adjust food prices to one common price level
     df_wfp = preproc.adjust_food_prices(country=country, df_wfp=df_wfp, data_source="WFP")
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Add market lon, lats to dataset"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
 
     # 2. Read CSV containing market coordinates and merge to price data
     df_wfp_with_coords = preproc.read_and_merge_wfp_market_coords(df_wfp=df_wfp, country=country)
@@ -49,11 +61,19 @@ def create_dataset(country, dropped_commodities):
     # PART B) Get climate part of data (SPEI)
     # ---------------------------------------
 
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Read climate data (SPEI)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
     df_spei = preproc.read_climate_data(time_slice=slice_time, long_slice=slice_lon, lat_slice=slice_lat,
                                         country=country)
 
     # PART C) Merge Outcomes of Part A) and Part C) and store them as excel
     # ---------------------------------------------------------------------
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Merge climate data to food data"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
     df_final = preproc.merge_food_price_and_climate_dfs(df_wfp_with_coords=df_wfp_with_coords, df_spei=df_spei)
 
     # Check that everything went okay in preprocessing/ merge part
@@ -61,6 +81,10 @@ def create_dataset(country, dropped_commodities):
         raise ValueError(f"Something went wrong in the preprocessing part.\n"
                          f"# rows of final/ merged df {df_final.shape[0]} should"
                          f" be the same as for the one of wfp {df_wfp.shape[0]}")
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Classify droughts"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
 
     df_final = preproc.classify_droughts(df_final)
 
@@ -72,8 +96,16 @@ def create_dataset(country, dropped_commodities):
     print(f"Counts Drought:\n{counts_drought}")
     print(f"Share of droughts: {counts_drought[True]}/ {(counts_drought[True] + counts_drought[False])}")
 
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Separate datasets droughts/ no droughts"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
+
     # Get the distinguished datasets
     df_drought, df_no_drought = preproc.separate_df_drought_non_drought(df_final)
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Write Summary Statistics 1 (for missings in prices & drought indicators/ SPEI)"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
 
     # Check missings
     preproc.summary_stats_prices_droughts(df_final=df_final)
@@ -95,14 +127,27 @@ def create_dataset(country, dropped_commodities):
     print(f"Dropping years: {years_to_drop}")
     df_final = preproc.drop_years(df_final=df_final, years_list=years_to_drop)
 
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Write summary statistics 2"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
     preproc.summary_stats_prices_droughts(df_final=df_final, excel_output_extension="-preproc-2")
 
     print("\n# ------------------------------------------------------------------------------------------------------\n"
           "# PREPROC: PHASE 2.2 (PRICES)"
           "\n# ------------------------------------------------------------------------------------------------------\n")
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # Continue preprocessing PER COMMODITY
+    # ------------------------------------------------------------------------------------------------------------------
+
     # 65 (30%), 60 (27%, 30% also for central region)
     cut_off_percentile = 60
+
+    # for commodity in df_final.Commodity.unique():
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Cut off missing values after certain percentile"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
 
     # Cut all regions with missing >= cut_off_percentile of missing values
     df_final = preproc.drop_missing_decile_per_region_prices(
@@ -110,6 +155,10 @@ def create_dataset(country, dropped_commodities):
                              f"sum-stats-preproc-2.xlsx",
         df_final=df_final,
         cut_off_percentile=cut_off_percentile, excel_output_extension=f"-{cut_off_percentile}p")
+
+    print("\n# ------------------------------------------------------------------------------------------------------\n"
+          "# PREPROC: Write summary statistics 3"
+          "\n# ------------------------------------------------------------------------------------------------------\n")
 
     # Write sum stats
     preproc.summary_stats_prices_droughts(df_final=df_final, excel_output_extension=f"-preproc-3-{cut_off_percentile}p")
@@ -123,7 +172,7 @@ def create_dataset(country, dropped_commodities):
     df_final = preproc.extrapolate_prices_regional_patterns(df_final=df_final, interpolation_method="linear")
 
     print("\n# ------------------------------------------------------------------------------------------------------\n"
-          "# PREPROC: Summary statistics"
+          "# PREPROC: Write summary statistics 4"
           "\n# ------------------------------------------------------------------------------------------------------\n")
 
     # Write sum stats
