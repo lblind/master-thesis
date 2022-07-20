66141067643c95751ac38ee0bbbd65d14dfb4333
L. Blind <l.blind@student.maastrichtuniversity.nl>
2022-07-20 11:15:42 +0200

    Mean per column better docum. + formatted

--- src/statistics_snippets.py
+++ src/statistics_snippets.py
@@ -6,23 +6,26 @@ import pandas as pd
 
 import utils
 
+
 def mean_per_column(gdf_final, group="District"):
     """
-    Calculates the mean per group (e.g. District, Region, etc.)
+    Calculates the mean per group (e.g. District, Region, etc.).
+    Creates a new column and adds it to the dataframe
 
-    :param group:
-    :return:
+    :param gdf_final: geopandas.DataFrame
+        Input dataframe
+    :param group: str
+        Column name of gdf_final on which to aggregate/ compute the means.
+    :return: gdf_merged: geopandas.DataFrame
     """
-    print(gdf_final.head())
-    print(gdf_final.District.unique())
 
     # calculate the means per group
-    df_means = gdf_final.groupby(group).apply(lambda x : x["AdjPrice"].mean())
+    df_means = gdf_final.groupby(group).apply(lambda x: x["AdjPrice"].mean())
 
     # convert pandas series to dataframe
-    df_means = pd.DataFrame(df_means)
+    df_means = pd.DataFrame({f"MeanAdjPricePer{group}": df_means})
 
-    # merge means colun to dataframe
-    gdf_merged = utils.merge_dfs_left(df_left=gdf_final,df_right= df_means, on=group)
+    # merge means column to dataframe
+    gdf_merged = utils.merge_dfs_left(df_left=gdf_final, df_right=df_means, on=group)
 
     return gdf_merged
