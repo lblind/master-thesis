b414428bd3f954cc52135b7d8c7abd71d846d792
L. Blind <l.blind@student.maastrichtuniversity.nl>
2022-08-13 11:39:50 +0200

    Added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='master-thesis',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='L. Blind',
+    author_email='l.blind@student.maastrichtuniversity.nl',
+    description='Implementation of Master Thesis Project'
+)
