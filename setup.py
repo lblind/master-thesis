aeebb92496037b78681236bd0fe34db7880276f3
L. Blind <l.blind@student.maastrichtuniversity.nl>
2022-08-13 11:44:29 +0200

    Updated Setup.py with dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(
     name='master-thesis',
     version='1.1',
-    packages=[''],
+    packages=['master-thesis'],
     url='',
     license='',
     author='L. Blind',
     author_email='l.blind@student.maastrichtuniversity.nl',
-    description='Implementation of Master Thesis Project'
+    description='Implementation of Master Thesis Project',
+    install_requires=['numpy', 'pandas', 'matplotlib', 'seaborn', 'openpyxl', 'xarray', 'geopy', 'missingno',
+                      'geopandas', 'pydmd']
 )
